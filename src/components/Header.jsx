35f0b5def98dc6731fb03f4d03845ce4d35280b2
Saksham Goyal <144555727+Sakshamm-Goyal@users.noreply.github.com>
2024-04-19 15:10:01 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
                 <div className='animated text-white text-sm'>
                 <AnimatedText/>
                     </div>
-                <div className='flex items-center gap-2 divide-x divide-white'>
+                <div className='flex items-center gap-8 divide-x divide-white'>
                     <div className='font-semibold text-white text-sm'><i className="ri-phone-fill"></i> +91 9092345600</div>
                     <div className='pl-2 text-white text-sm'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="currentColor"><path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM12.0606 11.6829L5.64722 6.2377L4.35278 7.7623L12.0731 14.3171L19.6544 7.75616L18.3456 6.24384L12.0606 11.6829Z"></path></svg></div>
                     <div className='font-semibold pl-1 text-white'>Get App</div>
