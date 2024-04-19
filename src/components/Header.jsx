f97249ed0fb1e0bfd763e4492fc1a0883e15e39f
Saksham Goyal <144555727+Sakshamm-Goyal@users.noreply.github.com>
2024-04-19 14:40:55 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
             </div>
             <div className='flex items-center shadow-md'>
                 <img className='px-4 py-3 w-36 h-28' src='/log.svg' />
-                <div className=' flex items-center gap-28 pl-24 rounded-l-md'>
+                <div className=' flex items-center gap-24 pl-24 rounded-l-md'>
                     <div className='relative flex'>
                         <div className='py-5 px-9 text-white bg-[#0Fa4Af] rounded-l-md'>
                             <div className='text-sm'>All Categories <i className="ri-arrow-down-s-fill"></i></div>
